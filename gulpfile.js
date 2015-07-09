7ff72707c6d920130a86158e1e44332358e71b23
張 偉 <zhang.wei@dragon.jp>
2015-07-09 20:17:26 +0900

    code beautify

--- gulpfile.js
+++ gulpfile.js
@@ -9,69 +9,34 @@ var rename = require('gulp-rename');
 var del = require('del');
 var convertEncoding = require('gulp-convert-encoding');
 
-var iterateFiles = require("iterate-files"),
-    path = require("path");
 var redis = require("redis");
 var sprintf = require('sprintf').sprintf;
 
 var csv = require('fast-csv');
-var fs = require('fs');
 var gutil = require('gulp-util');
 var Buffers = require('buffers');
 
-// Rerun the task download ken_all.zip
-gulp.task('download', function (cb) {
-    var rows = fs.readFileSync('download_urls.txt').toString().split("\n");
-    for (var i in rows) {
-        //download('http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/13tokyo.zip')
-        if (rows.hasOwnProperty(i)) {
-            download(rows[i])
-                .pipe(gulp.dest("downloads/"));
-        }
-    }
-    cb();
+gulp.task('clean', function (callback) {
+    del(['./dist/*', './downloads/*', './tmp/*'], callback);
 });
 
-// Rerun the task unzip ken_all.zip
-gulp.task('unzip', function (cb) {
-    // Load all javascript files in the test folder or any of their sub folders
-    iterateFiles(path.join(process.cwd(), "./downloads"), function (fileName) {
-        console.log(fileName);
-        return gulp.src(fileName)
-            .pipe(unzip())
-            .pipe(gulp.dest('./downloads/'));
-    }, function (err) {
-        // run code when all files have been found recursively
-        if (err) {
-            console.log(err);
-            cb(err);
-        }
-        cb();
-    }, /.zip$/)
+gulp.task('download', ['clean'], function (callback) {
+    download('http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip')
+        .pipe(gulp.dest("downloads/"))
+        .on('end', callback);
 });
 
-// Rerun the task unzip ken_all.zip
-gulp.task('convert_encoding', function (cb) {
-    // Load all javascript files in the test folder or any of their sub folders
-    iterateFiles(path.join(process.cwd(), "./downloads"), function (fileName) {
-        console.log(fileName);
-        return gulp.src(fileName)
-            .pipe(convertEncoding({from: "cp932", to: "utf8"}))
-            .pipe(rename({extname: '.csv'}))
-            .pipe(gulp.dest('./tmp/'));
-    }, function (err) {
-        // run code when all files have been found recursively
-        if (err) {
-            console.log(err);
-            cb(err);
-        }
-        cb();
-    }, /.CSV$/)
-
+gulp.task('prepare', ['download'], function (callback) {
+    gulp.src('./downloads/*.zip')
+        .pipe(unzip())
+        .pipe(convertEncoding({from: "cp932", to: "utf8"}))
+        .pipe(rename({extname: '.csv'}))
+        .pipe(gulp.dest('./tmp/'))
+        .on('end', callback);
 });
 
-gulp.task('convert2redis_command', function (cb) {
-    return gulp.src('./tmp/*.csv')
+gulp.task('export2redis', ['prepare'],  function (callback) {
+    gulp.src('./tmp/*.csv')
         .pipe(through2.obj(function (file, enc, callback) {
             var store = Buffers();
 
@@ -79,7 +44,8 @@ gulp.task('convert2redis_command', function (cb) {
                 .fromStream(file)
                 .on("data", function (record) {
                     var line = sprintf(
-                        "HMSET jp_zip::%s state %s city %s address %s\n",
+                        "HMSET jp:zip:%s zipcode %s state %s city %s address %s\n",
+                        record[2],
                         record[2],
                         record[6],
                         record[7],
@@ -91,14 +57,10 @@ gulp.task('convert2redis_command', function (cb) {
                     callback(null, file);
                     gutil.log('convert2redis:', gutil.colors.green('✔ ') + file.relative);
                 });
-
-
         }))
         .pipe(rename({basename: 'aloha', extname: '.txt'}))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./dist/'))
+        .on('end', callback);
 });
 
-gulp.task('clean', function (cb) {
-    // You can use multiple globbing patterns as you would with `gulp.src`
-    del(['./dist/*', './downloads/*', './tmp/*'], cb);
-});
+gulp.task('default', ['export2redis']);
